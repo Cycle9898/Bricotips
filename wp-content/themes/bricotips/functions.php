ad12157a272fdc663e3be4fdbf23b94bff2c40d2
Cycle9898 <cycle9898@gmail.com>
2025-01-24 17:33:28 +0100

    create child theme Bricotips

---
+++ wp-content/themes/bricotips/functions.php
@@ -0,0 +1,9 @@
+<?php
+// load theme scripts
+add_action('wp_enqueue_scripts', 'theme_enqueue_styles');
+
+function theme_enqueue_styles()
+{
+    // load parent theme style
+    wp_enqueue_style('parent-style', get_template_directory_uri() . '/style.css');
+}
