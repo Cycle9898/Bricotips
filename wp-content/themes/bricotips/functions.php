47dae4bbbafea4aa27f38471ffd29149a4bb4ea2
Cycle9898 <cycle9898@gmail.com>
2025-01-27 16:05:44 +0100

    add additional stylesheet on homepage

--- wp-content/themes/bricotips/functions.php
+++ wp-content/themes/bricotips/functions.php
@@ -6,4 +6,6 @@ function theme_enqueue_styles()
 {
     // load parent theme style
     wp_enqueue_style('parent-style', get_template_directory_uri() . '/style.css');
+    // load additional style(s)
+    wp_enqueue_style('theme-style', get_stylesheet_directory_uri() . '/CSS/theme.css', array(), filemtime(get_stylesheet_directory() . '/CSS/theme.css'));
 }
